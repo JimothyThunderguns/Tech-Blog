acd9c146ac16935490078ddd14e4318bf2908cf8
Ryan Jackson <rcjackson96@gmail.com>
2022-12-07 15:13:42 -0600

    server code and hanlebars

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const express = express();
+const PORT = process.env.PORT || 3001
+
+const hbs = 
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+});
